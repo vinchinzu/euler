e2301e53f54991b25de0678088b0a24474019beb
rockhog <lucas.vinze@rockhog.com>
2014-10-29 15:33:33 -0500

    add 60

--- 60.rb
+++ 60.rb
@@ -10,3 +10,56 @@
 # to produce another prime.
 
 
+
+
+require 'Prime'
+
+set = Prime.first(120)
+
+test = [3,7,109,673]
+
+base = []
+set.each do |a|
+	set.each do |b|
+	 
+	 if (a+b).prime?
+	  base << a + b
+	 end
+	end
+end
+base
+
+
+base.each do |a| 
+  base.each do |b|
+    x = (a.to_s + b.to_s).to_i
+	if x.prime?
+	 puts x
+	end
+	end
+end
+
+
+test = [3,7,109,673]
+test << 43
+test
+
+cheq_set(test)
+
+def check_array_primes(arr)
+  arry{is.prime
+  arr.all?{|x| x.isprime?}
+end
+
+def cheq_set(arr)
+ c_arr = []
+ arr.each do |a|
+   arr.each do |b|
+     x = (a.to_s + b.to_s).to_i
+	if x.prime?
+	 c_arr << x
+    end
+   end
+ end
+end
+
