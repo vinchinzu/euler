bd33de9ec48a2d2c4f8320ee65042ea7474d6cc1
Vinze <lvinze@deloitte.com>
2016-05-03 16:26:39 -0400

    new 14

--- solutions/14.rb
+++ solutions/14.rb
@@ -1,22 +1,57 @@
-#longest collatz
+# Longest Collatz sequence
+# Problem 14
+# The following iterative sequence is defined for the set of positive integers:
 
-# n = 3n+1 if n odd?
-# n = 2n if n even?
+# n → n/2 (n is even)
+# n → 3n + 1 (n is odd)
+
+# Using the rule above and starting with 13, we generate the following sequence:
+
+# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+# It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. 
+#Although it has not been proved yet (Collatz Problem), 
+#it is thought that all starting numbers finish at 1.
+
+# Which starting number, under one million, produces the longest chain?
+
+# NOTE: Once the chain starts the terms are allowed to go above one million.
+
+#gensequence
+
+
+def col(test)
+  if test%2==0 
+  out = test/2
+  else 
+ out = test*3 + 1
+ end
+out
+end
+
+max = 1
+top = 1
+start  = 750000
+
+while (start < 1_000_000)
+ set = []
+ set << start
+ nex = col (start)
+  while (nex != 1) 
+   nex = col(nex)
+   set << nex
+  end
  
+if set.count > max then 
+  max = set.count
+  top = start
+end
 
-# def calc_seq(n,s)
-  # if n==1
-  # return s
-# elsif n & 1 ==0
-# return calc_seq(n/2,s+1)
-# else
-# return calc_seq(3*n+1, s+1)
-# end
-# end
+start +=1
 
-# seq = (1..1000000).map do |i|
-# [i, calc_seq(i)]
-# end
-# puts seq.max by {|i| i[1]}
+
+#puts start,max, set.count
+end
+
+puts top
 
 
