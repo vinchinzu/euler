e428d24319db6ab17ffcfa7a1f66ca69d42577f5
Vinze <lukevinze@gmail.com>
2016-05-10 09:15:39 -0400

    weekend

--- 21.rb
+++ 21.rb
@@ -8,3 +8,47 @@
  # The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
 # # Evaluate the sum of all the amicable numbers under 10000.
+
+#find all divisiors
+
+def div(n)
+  set = []
+  (1..n).each do |x|
+   n%x == 0 ? set << n/x : next
+   end 
+   set
+end 
+
+def d(n)
+  t = div(n)
+  t.shift
+  t.inject(:+)
+end
+
+amic = []
+(2..10000).each do |n|
+  a = d(n)
+  b = d(a)
+ if n == b && n != a then 
+   amic << [n, a]
+  else 
+ end
+end
+ 
+ puts amic.flatten.uniq.inject(:+)
+ 
+ 
+  
+
+
+# class Integer
+  # def divisor_count
+    # sum = 1
+    # # prime_division return two dimensional array.
+    # # for 48, [3,1], [2,4] is the result
+    # self.prime_division.each do |x| 
+      # sum *= (x[1] + 1)  
+    # end
+    # sum
+  # end
+# end
--- 31.rb
+++ 31.rb
@@ -5,19 +5,46 @@
 # 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
 # It is possible to make £2 in the following way:
 
-# 1£1 + 150p + 220p + 15p + 12p + 31p
+# 1£1 + 1 50p + 2 20p + 1 5p + 1 2p + 3 1p
 # How many different ways can £2 be made using any number of coins?
 
 set = [1,2,5,10,20,50,100,200]
+
+
+a=200
+b=0
+
+arr = [a*1,b*2] 
+
+while a != 0
+  a -= 1
+  b += 1
+  puts a, b, combos
+  combos +=1
+end
+
+
+
 max = 200
  
 sum = 0
 combos = 0
 
+
+max = set[0] * 
+
 while sum < max
   coins = []
   coins << set[0]
+  puts coins
+  
   sum = coins.inject(:+)
 end
 
 coins
+
+#maybe a hash map/dictionary
+#how to map a-z to 1-26
+
+
+
